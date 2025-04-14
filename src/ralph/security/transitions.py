62ca5ce24f5b7f3d82a869ce68b592ab3bcd7632
Paweł Szulc <pawel.szulc@allegro.com>
2025-04-14 12:48:51 +0200

    Fixes for django 3.1

--- src/ralph/security/transitions.py
+++ src/ralph/security/transitions.py
@@ -4,7 +4,6 @@ Transition actions connected with security.
 
 """
 
-from django.db import transaction
 from django.utils.translation import gettext_lazy as _
 
 from ralph.data_center.models import DataCenterAsset
